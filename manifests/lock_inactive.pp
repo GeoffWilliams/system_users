1e1a2e1c84c043cfaaa345d90cc993ecd0a95473
Geoff Williams <geoff.williams@puppetlabs.com>
2017-01-23 01:04:21 +1100

    shut up the linter till i figure out how to shut it up..

--- manifests/lock_inactive.pp
+++ manifests/lock_inactive.pp
@@ -5,9 +5,9 @@
 # @param period How long to wait before locking a user (days)
 class system_users::lock_inactive($period = 30) {
   file_line { "lock inactive users":
-    ensure  => present,
-    match   => 'INACTIVE=',
-    path    => '/etc/default/useradd',
-    line    => "INACTIVE=${period}",
+    ensure => present,
+    match  => 'INACTIVE=',
+    path   => '/etc/default/useradd',
+    line   => "INACTIVE=${period}",
   }
 }
