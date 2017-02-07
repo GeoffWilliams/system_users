e37621eec57061bdba66ad5cf08c1fbe7b92a8d0
Geoff Williams <geoff.williams@puppetlabs.com>
2017-02-07 10:01:20 +0800

    fix error about missing fact if under test

--- manifests/lock_empty_password.pp
+++ manifests/lock_empty_password.pp
@@ -4,8 +4,12 @@
 #
 # @param lock_method string to insert into password field to lock the account
 class system_users::lock_empty_password($lock_method = '*') {
-
-  user { $user_audit['empty_password']:
-    password => $lock_method
+  $users_to_lock = dig($facts, 'user_audit', 'empty_password')
+  if $users_to_lock {
+    user { $users_to_lock:
+      password => $lock_method,
+    }
+  } else {
+    warm("user_audit fact missing - system_users module seems broken?")
   }
 }
