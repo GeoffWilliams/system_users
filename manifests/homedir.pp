106a77f16a36a68dd97768c192e2e8f86276d057
Geoff Williams <geoff.williams@puppetlabs.com>
2017-02-16 05:30:34 +0800

    protect call to iterate homedirs - blank in onceover

--- manifests/homedir.pp
+++ manifests/homedir.pp
@@ -3,24 +3,23 @@
 # Check and fix common gotchas in user homedirs on this system:
 #
 class system_users::homedir {
-  $homedirs = $facts['user_audit']['homedirs']
-  $homedirs.filter |$user, $hash| {
-    ! $user in $facts['user_audit']['low_uids'] and
-    $user != "root" and
-    ! $hash['path'] in ["/", "/bin", "/usr/bin", "/usr/sbin", "/sbin", "/dev/null"]
-  }.each |$user, $hash| {
-    # do not attempt permission corrections as doing so may trash the system.
-    # Also, skip low uids as these users often share vital system directories
-    # between themselves such as /sbin, /, /var/lib, etc...
-    $res = {
-      $hash['path'] => {
-        "ensure" => "directory",
-        "owner"  => $user,
+  $homedirs = dig($facts, 'user_audit', 'homedirs')
+  if $homedirs {
+    $homedirs.filter |$user, $hash| {
+      ! $user in $facts['user_audit']['low_uids'] and
+      $user != "root" and
+      ! $hash['path'] in ["/", "/bin", "/usr/bin", "/usr/sbin", "/sbin", "/dev/null"]
+    }.each |$user, $hash| {
+      # do not attempt permission corrections as doing so may trash the system.
+      # Also, skip low uids as these users often share vital system directories
+      # between themselves such as /sbin, /, /var/lib, etc...
+      $res = {
+        $hash['path'] => {
+          "ensure" => "directory",
+          "owner"  => $user,
+        }
       }
+      ensure_resources("file", $res)
     }
-    ensure_resources("file", $res)
-
-    # }
-#  SOL10X_96	Check User Dot File Permissions
   }
 }
