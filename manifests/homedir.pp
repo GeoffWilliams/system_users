79393c04c59c2cf203b6ddc0d62b1b9473c497a3
Geoff Williams <geoff@geoffwilliams.me.uk>
2018-06-18 14:47:42 +1000

    redo support for homedirs to exlude more directories and system users

--- manifests/homedir.pp
+++ manifests/homedir.pp
@@ -2,24 +2,42 @@
 #
 # Check and fix common gotchas in user homedirs on this system:
 #
-class system_users::homedir {
+class system_users::homedir(
+    $mode = unset,
+) {
   $homedirs = dig($facts, 'user_audit', 'homedirs')
+
   if $homedirs {
+    # skip system uids as these users often share vital system directories
+    # between themselves such as /sbin, /, /var/lib, etc...
     $homedirs.filter |$user, $hash| {
-      ! $user in $facts['user_audit']['low_uids'] and
+      ! $user in $facts['user_audit']['system_uids'] and
       $user != "root" and
-      ! $hash['path'] in ["/", "/bin", "/usr/bin", "/usr/sbin", "/sbin", "/dev/null"]
+      ! (
+        $hash['path'] =~ /^\/bin/ or
+        $hash['path'] =~ /^\/boot/ or
+        $hash['path'] =~ /^\/dev/ or
+        $hash['path'] =~ /^\/etc/ or
+        $hash['path'] =~ /^\/lib/ or
+        $hash['path'] =~ /^\/media/ or
+        $hash['path'] =~ /^\/mnt/ or
+        $hash['path'] =~ /^\/opt/ or
+        $hash['path'] =~ /^\/proc/ or
+        $hash['path'] =~ /^\/run/ or
+        $hash['path'] =~ /^\/sbin/ or
+        $hash['path'] =~ /^\/srv/ or
+        $hash['path'] =~ /^\/sys/ or
+        $hash['path'] =~ /^\/tmp/ or
+        $hash['path'] =~ /^\/usr/ or
+        $hash['path'] =~ /^\/var/
+      )
     }.each |$user, $hash| {
-      # do not attempt permission corrections as doing so may trash the system.
-      # Also, skip low uids as these users often share vital system directories
-      # between themselves such as /sbin, /, /var/lib, etc...
-      $res = {
-        $hash['path'] => {
-          "ensure" => "directory",
-          "owner"  => $user,
-        }
+
+      file { $hash['path']:
+        ensure => directory,
+        owner  => $user,
+        mode   => $mode,
       }
-      ensure_resources("file", $res)
     }
   }
 }
